0b5d583ebd48cba823c94deddb203701988b2982
Martynas Paulikas <martynas.paulikas02@gmail.com>
2017-11-22 14:57:11 +0000

    implemented basic three.js model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  camera: THREE.PerspectiveCamera;
+  scene: THREE.Scene;
+  renderer: THREE.WebGLRenderer;
+  geometry: THREE.BoxGeometry;
+  material: THREE.MeshNormalMaterial;
+  mesh: THREE.Mesh;
+
+  ngOnInit() {
+
+    this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+    this.camera.position.z = 1;
+
+    this.scene = new THREE.Scene();
+
+    this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    this.material = new THREE.MeshNormalMaterial();
+
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
+
+    this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+    this.animate();
+  }
+
+  animate() {
+
+    requestAnimationFrame(this.animate);
+
+    this.mesh.rotation.x += 0.01;
+    this.mesh.rotation.y += 0.02;
+
+    this.renderer.render(this.scene, this.camera);
+
+  }
+
+
 }
