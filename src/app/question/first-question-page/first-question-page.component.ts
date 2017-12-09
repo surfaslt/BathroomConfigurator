1db00d42c4bec487a9b1bcc655b392c9b669a213
surfaslt <map00054@students.stir.ac.uk>
2017-12-09 18:05:58 +0000

    Fixed typo in questions

--- src/app/question/first-question-page/first-question-page.component.ts
+++ src/app/question/first-question-page/first-question-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-first-question-page',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-question-page.component.css']
 })
 export class FirstQuestionPageComponent implements OnInit {
+
+  @Output() onVoted = new EventEmitter<boolean>();
 
   constructor() { }
 
