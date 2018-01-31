31b58bec5abfd87464ec67231049c669e56697a6
Martynas Paulikas <martynas.paulikas02@gmail.com>
2018-01-31 16:29:59 +0000

    attempted to run cucumber, failed.

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,89 +1,48 @@
-// cucumber setup by protractor-cucumber-framework readme.md file
-exports.config = {
-  // set to "custom" instead of cucumber.
-  framework: 'custom',
-
-  // path relative to the current config file
-  frameworkPath: require.resolve('protractor-cucumber-framework'),
-
-  // require feature files
-  specs: [
-    'e2e/features/**/*.feature' // accepts a glob
-  ],
-
-  cucumberOpts: {
-    // require step definitions
-    require: [
-      'e2e/features/stepDefinitions/*.steps.js' // accepts a glob
-    ]
-  }
-}
-
-
-// cucumber setup
-/*
-
-exports.config = {
-  //seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
-  getPageTimeout: 60000,
-  //allScriptsTimeout: 11000,
-  allScriptsTimeout: 500000,
-  framework: 'custom',
-  // path relative to the current config file
-  frameworkPath: require.resolve('protractor-cucumber-framework'),
-  capabilities: {
-    'browserName': 'chrome'
-  },
-
-  // Spec patterns are relative to this directory.
-  specs: [
-    'e2e/features/!**!/!*.feature'
-  ],
-
-  //directConnect: true,
-
-  baseUrl: 'http://localhost:4200/',
-
-  cucumberOpts: {
-    require: 'e2e/features/stepDefinitions/!**!/!*.steps.js',
-    tags: false,
-    //format: 'pretty',
-    profile: false,
-    'no-source': true
-  }
-};
-*/
-
-/*
-
-// Jasmine setup
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const {SpecReporter} = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
-  specs: [
-    './e2e/!**!/!*.e2e-spec.ts'
-  ],
   capabilities: {
     'browserName': 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function () {
-    }
+
+  // Specs here are the cucumber feature files
+  specs: [
+    './e2e/features/*.feature'
+  ],
+
+  // Use a custom framework adapter and set its relative path
+  framework: 'custom',
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
+
+  // cucumber command line options
+  cucumberOpts: {
+    // require step definition files before executing features
+    require: ['./e2e/features/stepDefinitions/*.js'],
+    // <string[]> (expression) only execute the features or scenarios with tags matching the expression
+    tags: [],
+    // <boolean> fail if there are any undefined or pending steps
+    strict: true,
+    // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
+    format: [
+      'progress',
+      'pretty:reports/summary.txt',
+      'json:reports/summary.json'
+    ],
+    // <boolean> invoke formatters without executing steps
+    dryRun: false,
+    // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
+    compiler: []
   },
+
+  // Enable TypeScript for the tests
   onPrepare() {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
-    jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
   }
 };
-*/
