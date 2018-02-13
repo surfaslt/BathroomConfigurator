7f35956571e625c61a40efe01160958f2270460b
Martynas Paulikas <martynas.paulikas02@gmail.com>
2018-02-13 10:55:07 +0000

    Missing cucumbre stuff which I forgot to add to previous commit.

---
+++ e2e/support/hooks.js
@@ -0,0 +1,31 @@
+"use strict";
+const {browser, by, element} = require("protractor");
+const {Cucumber, BeforeAll, After, Status} = require("cucumber");
+
+/**
+ * Go the angular starter page before to start eah test
+ */
+BeforeAll({timeout: 30 * 1000}, function (callback) {
+  browser.get('http://localhost:4200').then(()=> {
+    callback();
+  });
+});
+
+/**
+ * Logout after finish the execution of each test to start from initial application state
+ */
+After(function (scenario) {
+  if (scenario.result.status === Status.FAILED) {
+    const attach = this.attach; // cucumber's world object has attach function which should be used
+    return browser.takeScreenshot().then(function (png) {
+      const decodedImage = new Buffer(png, "base64");
+      return attach(decodedImage, "image/png");
+    });
+  }
+  else {
+    // logout before to start the next test
+    return element(by.id('linkLogout')).click();
+  }
+});
+
+
