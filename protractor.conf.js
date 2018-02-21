7c1767a6b9783479f065a1e25a8a2aed5f140b43
surfaslt <map00054@students.stir.ac.uk>
2018-02-21 22:22:14 +0000

    Tried to fix cucumber -failed.

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,19 +1,48 @@
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+
 exports.config = {
-  // set to "custom" instead of cucumber.
-  framework: 'custom',
+  allScriptsTimeout: 11000,
+  capabilities: {
+    'browserName': 'chrome'
+  },
+  directConnect: true,
+  baseUrl: 'http://localhost:4200/',
 
-  // path relative to the current config file
-  frameworkPath: require.resolve('protractor-cucumber-framework'),
-
-  // require feature files
+  // Specs here are the cucumber feature files
   specs: [
-    './e2e/features/**/*.feature' // accepts a glob
+    './e2e/features/*.feature'
   ],
 
+  // Use a custom framework adapter and set its relative path
+  framework: 'custom',
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
+
+  // cucumber command line options
   cucumberOpts: {
-    // require step definitions
-    require: [
-      './e2e/features/step_definitions/**/*.steps.js' // accepts a glob
-    ]
+    // require step definition files before executing features
+    require: ['./e2e/steps/**/*.ts'],
+    // <string[]> (expression) only execute the features or scenarios with tags matching the expression
+    tags: [],
+    // <boolean> fail if there are any undefined or pending steps
+    strict: true,
+    // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
+    format: [
+      'pretty',
+      'pretty:reports/summary.txt',
+      'json:reports/summary.json'
+    ],
+    // <boolean> invoke formatters without executing steps
+    dryRun: false,
+    // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
+    compiler: []
+  },
+
+  // Enable TypeScript for the tests
+  onPrepare() {
+    require('ts-node').register({
+      project: 'e2e/tsconfig.e2e.json'
+    });
   }
 };
