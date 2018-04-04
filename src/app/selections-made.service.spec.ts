5303c54d0c5411b5ffb11f6081bf724653616c07
surfaslt <map00054@students.stir.ac.uk>
2018-04-04 08:45:05 +0100

    Uncommented selections-made.service tests

--- src/app/selections-made.service.spec.ts
+++ src/app/selections-made.service.spec.ts
@@ -2,24 +2,6 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { SelectionsMadeService } from './selections-made.service';
 
-xdescribe("SelectionsMadeService", () => {
-
-  it("it should pass without any issues", () => {
-    let a = 12;
-    expect(a).toBe(12);
-  });
-
-  it("it should not pass as the values are undefined", () => {
-    let u;
-    expect(u).toBeDefined('u was not defined, so this test FAILED');
-  });
-
-  it("without any expectation.", () => {
-
-  });
-
-});
-/*
 describe('SelectionsMadeService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -31,4 +13,17 @@ describe('SelectionsMadeService', () => {
     expect(service).toBeTruthy();
   }));
 });
-*/
+
+xdescribe("SelectionsMadeService", () => {
+
+  it("it should pass without any issues", () => {
+    let a = 12;
+    expect(a).toBe(12);
+  });
+
+  it("it should not pass as the values are undefined", () => {
+    let u;
+    expect(u).toBeDefined('u was not defined, so this test FAILED');
+  });
+  it("without any expectation.", () => {});
+});
