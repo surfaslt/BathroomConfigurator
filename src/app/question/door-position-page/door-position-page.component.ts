2456f7e7f84893f2ac7e22842cc0b39ec9032fe2
surfaslt <map00054@students.stir.ac.uk>
2018-04-17 18:20:09 +0100

    added comments to question component and door-position component. Removed ngOnInit from question component

--- src/app/question/door-position-page/door-position-page.component.ts
+++ src/app/question/door-position-page/door-position-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+/**
+ * this component allows user to choose where in the room his doors are and whether they open inwards or outwards
+ */
+import { Component, EventEmitter, Output } from '@angular/core';
 import {SelectionsMadeService} from "../../selections-made.service";
 import {HelperService} from "../../helper.service";
 
@@ -7,29 +10,28 @@ import {HelperService} from "../../helper.service";
   templateUrl: './door-position-page.component.html',
   styleUrls: ['./door-position-page.component.css']
 })
-export class DoorPositionPageComponent implements OnInit {
+export class DoorPositionPageComponent {
 
-  @Output() onChangeMade: EventEmitter<string> = new EventEmitter<string>();
+  @Output() onChangeMade: EventEmitter<string> = new EventEmitter<string>(); // notifies question component about change made
 
-  public doorPositionOptions: string[] = ['Left', 'Middle', 'Right'];
-  public doorOpeningType: string;
-  public doorPositionChosen: string;
+  public doorPositionOptions: string[] = ['Left', 'Middle', 'Right']; // basic door positions in the room
+  public doorOpeningType: string; // stores whether doors open inwards or outwards
+  public doorPositionChosen: string; // stores which door position was selected by the user
 
   constructor(private selectionsMadeService: SelectionsMadeService, private helperService: HelperService) {
-    helperService.setProgress(3);
+    helperService.setProgress(3); // update progress bar
+    // pre-set selections from data in service class
     this.doorOpeningType = selectionsMadeService.getDoorOpeningType();
     this.doorPositionChosen = selectionsMadeService.getDoorPosition();
   }
 
-  ngOnInit() {
-  }
-
+  // sets the door opening type to the users selected opening type and notifies 3D component about it (through question component)
   doorOpeningTypeChanged = ():void => {
-    console.log(this.doorOpeningType);
     this.selectionsMadeService.setDoorOpeningType(this.doorOpeningType);
     this.onChangeMade.emit('doorOpeningTypeChanged');
   }
 
+  // sets the door position to the users selected position and notifies 3D component about it (through question component)
   doorPositionChanged = ():void => {
     this.selectionsMadeService.setDoorPosition(this.doorPositionChosen);
     this.onChangeMade.emit('doorPositionChanged');
--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,3 +1,7 @@
+/**
+ * This component holds other components that asks users relative questions and also 3D component displaying users
+ * choices. This component also acts as a bridge of communication between the prior and the latter.
+ */
 import { Component, AfterViewInit, OnDestroy, ViewChild } from '@angular/core';
 import { SelectionsMadeService } from './../selections-made.service';
 import { HelperService } from './../helper.service';
@@ -12,18 +16,20 @@ export class QuestionComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('productsModal') productsModal: ModalDirective;
 
-  public currentQuestionNo: number = 1;
-  public totalQuestionNo: number = 5;
-  public pageChange: string = '';
+  public currentQuestionNo: number = 1; // Holds value of which question no. to show
+  public totalQuestionNo: number = 5; // Used to toggle "next page" button in the view
+  public pageChange: string = ''; // holds the change made in the page to be passed to 3D component
+  // The below pre-populates product selection modal with images of products.
   public smallProductImagePaths: string[] = ['cupboard1.png', 'cupboard1.jpg', 'cupboard1.png'];
   public mediumProductImagePaths: string[] = ['cupboard2.png', 'cupboard2.jpg', 'cupboard2.png', 'cupboard2.png'];
   public largeProductImagePaths: string[] = ['cupboard3.png', 'cupboard3.jpg'];
-  public smallProductWidthNeeded: number = 400;
-  public mediumProductWidthNeeded: number = 500;
-  public largeProductWidthNeeded: number = 600;
-  private productImagesFolderPath: string;
-  private messagesToDynamicCanvasComponent: string[] = ['showRoomDimensionsElements','showDoorPositionElements','showTubParametersElements','showPlaceholderElements','showSummaryElements'];
-
+  public smallProductWidthNeeded: number = 400; // constant for small product size
+  public mediumProductWidthNeeded: number = 500; // constant for medium product size
+  public largeProductWidthNeeded: number = 600; // constant for large product size
+  private productImagesFolderPath: string; // shortcut to product images folder
+  // messages that are passed to 3D component
+  private messagesToDynamicCanvasComponent: string[] = ['showRoomDimensionsElements','showDoorPositionElements',
+    'showTubParametersElements','showPlaceholderElements','showSummaryElements'];
 
   constructor(private selectionsMadeService: SelectionsMadeService, public helperService:HelperService) {
     this.currentQuestionNo = helperService.getCurrentQuestionNo();
@@ -31,18 +37,31 @@ export class QuestionComponent implements AfterViewInit, OnDestroy {
   }
 
   ngAfterViewInit(){
-    console.log("Question component, change will be made: ", this.messagesToDynamicCanvasComponent[this.currentQuestionNo-1]);
+    // updates 3D world to display relevant info to the current page
     this.pageChangeMade(this.messagesToDynamicCanvasComponent[this.currentQuestionNo-1]);
   }
 
+  /**
+   * forced 3D component to update itself even if its input value is the same as it was
+   * this is needed because the data in the service classes might have updated and so the view needs to be refreshed
+   */
   pageChangeMade = (changedName: string): void => {
     this.pageChange = this.pageChange == changedName ? changedName + '1' : changedName; // triggers ngOnChanges() in dynamic-canvas component
   }
 
+  /**
+   * receives messages from 3D component: when placeholder is clicked - displays modal with products
+   * @param {string} changedName
+   */
   canvasChangeMade = (changedName: string): void => {
     this.showProductModal();
   }
 
+  /**
+   * gets triggered when product is selected in the modal. Puts the selected product into helperService class and
+   * notifies the 3D component
+   * @param {string} imageSrc
+   */
   productChosen = (imageSrc:string):void => {
     let splittedSrc:string[] = imageSrc.split("/");
     let productName:string = splittedSrc[splittedSrc.length - 1].split('.')[0];
@@ -51,24 +70,42 @@ export class QuestionComponent implements AfterViewInit, OnDestroy {
     this.hideProductsModal();
   }
 
+  /**
+   * shows popup window with available products to replace the placeholder clicked
+   */
   showProductModal = (): void => {
     this.productsModal.show();
   }
 
+  /**
+   * closes the popup window with available products to replace the placeholder clicked
+   */
   hideProductsModal = (): void => {
     this.productsModal.hide();
   }
 
+  /**
+   * changes the page forwards and notifies 3D component to update itself accordingly
+   */
   nextQuestion = (): void => {
     this.currentQuestionNo++;
     this.pageChangeMade(this.messagesToDynamicCanvasComponent[this.currentQuestionNo-1]);
   }
 
+  /**
+   * changes the page backwards and notifies 3D component to update itself accordingly
+   */
   previousQuestion = ():void => {
     this.currentQuestionNo--;
     this.pageChangeMade(this.messagesToDynamicCanvasComponent[this.currentQuestionNo-1]);
   }
 
+  /**
+   * when user goes out of question component, it records the page number that the user was on so that next time the
+   * user is on question component, the component displays the same page.
+   * this method was essential for navigation when summary page component was not underneath question component
+   * and now it is left in case it is needed again
+   */
   ngOnDestroy(): void {
     this.helperService.setCurrentQuestionNumber(this.currentQuestionNo);
   }
